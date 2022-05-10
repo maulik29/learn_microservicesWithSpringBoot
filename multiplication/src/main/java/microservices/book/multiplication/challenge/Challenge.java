8cfecdc115822caf280c1fc666343000a7cb2d1b
Maulik Munshi <maulik.munshi@gmail.com>
2022-05-10 21:43:57 +0530

    Initial commit

---
+++ multiplication/src/main/java/microservices/book/multiplication/challenge/Challenge.java
@@ -0,0 +1,13 @@
+package microservices.book.multiplication.challenge;
+
+import lombok.*;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Challenge {
+
+	private int factorA;
+	private int factorB;
+}
